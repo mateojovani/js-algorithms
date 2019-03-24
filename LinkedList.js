ae358558c4aac844d58c80c3664090adf92ed18f
mjovani <mjovani@helius-software.com>
2019-03-24 19:43:20 +0100

    Linked List and Stack

---
+++ LinkedList.js
@@ -0,0 +1,110 @@
+function Node (data) {
+    this.data = data
+    this.prev = null
+    this.next = null
+}
+
+function List () {
+    this.head = null
+    this.length = 0
+
+    this.get = (index) => {
+        let current = this.head
+        let i = 0
+        if (index >= this.length)
+            index = this.length - 1
+
+        while (current && i !== index) {
+            current = current.next
+            i++
+        }
+
+        return current
+    }
+
+    this.add = (data, index) => {
+        let node = new Node(data)
+        let current = this.head
+        let length = this.length
+        let i = 0
+        this.length++
+        if (!current) //empty list
+            return this.head = node
+
+        if (index && index >= length)
+            index = length - 1
+
+        if (index != undefined) {
+            while (current && i < index) {
+                current = current.next
+                i++
+            }
+
+            if (length > 0 && index === 0) { //add to head
+                current.prev = node
+                node.next = current
+                this.head = node
+            } else { //add inside
+                node.next = current.next
+                current.next = node
+                node.prev = current
+            }
+        } else { //add to tail
+            while (current.next)
+                current = current.next
+
+            current.next = node
+            node.prev = current
+        }
+    }
+
+    this.remove = (index = 0) => {
+        let i = 0;
+        if (index >= this.length)
+            index = this.length - 1
+
+        let current = this.head
+
+        if(index === 0) { // head
+            this.head = current.next
+            this.head.prev = null
+        } else if (index === this.length - 1) {
+            while (current.next)
+                current = current.next
+            current.prev.next = null
+        } else {
+            while (current && i !== index) {
+                current = current.next
+                i++
+            }
+            let prev = current.prev, next = current.next
+            prev.next = next
+            next.prev = prev
+            current = null
+        }
+
+        this.length--
+    }
+
+    this.toArray = () => {
+        let arr = new Array(this.length).fill(0)
+        return arr.map((_, index) => this.get(index).data)
+    }
+}
+
+let list = new List()
+list.add(1)
+list.add(2)
+list.add(3)
+list.add(5)
+list.add(4, 2)
+list.add(0, 0)
+list.add(20, 6)
+list.add(-1, 0)
+list.add(21)
+list.remove(7)
+list.remove(0)
+list.remove(9)
+list.add(6)
+
+console.log(list.toArray())
---
+++ Stack.js
@@ -0,0 +1,71 @@
+function Node (data) {
+    this.next = null
+    this.data = data
+}
+
+function Stack () {
+    this.top = null
+    this.length = 0
+
+    this.push = (data) => {
+        if (!this.top)
+            this.top = new Node(data)
+        else {
+            let currentTop = this.top
+            this.top = new Node(data)
+            this.top.next = currentTop
+        }
+
+        this.length++
+    }
+
+    this.pop = () => {
+        let currentTop = this.top
+        this.top = currentTop.next
+        this.length--
+        return currentTop ? currentTop.data : null
+    }
+
+    this.peek = () => this.top ? this.top.data: null
+}
+
+// Set of Stacks
+function SetOfStacks (stackThreshold = 3) {
+    this.stacks = [new Stack()]
+
+    this.push = (data) => {
+        let currentStack = this.stacks[this.stacks.length - 1]
+        if (currentStack.length >= stackThreshold) {
+            currentStack = new Stack()
+            this.stacks.push(currentStack)
+        }
+
+        currentStack.push(data)
+    }
+
+    this.pop = () => {
+        let currentStack = this.stacks[this.stacks.length - 1]
+        let data = currentStack.pop()
+
+        if(currentStack.length === 0)
+            this.stacks.pop()
+
+        return data
+    }
+
+    this.peek = () => this.stacks.length ? this.stacks[this.stacks.length - 1].peek(): null
+}
+
+let st = new SetOfStacks(3)
+
+st.push(34)
+st.push(1)
+st.push(4)
+st.push(5)
+console.log(st.peek())
+st.pop()
+st.pop()
+console.log(st.peek())
+st.pop()
+st.pop()
+console.log(st.peek())
