9e1efd3f6a7a20e51c25931aa658bf9faa5acb19
mjovani <mjovani@helius-software.com>
2019-04-08 19:55:29 +0200

    Added Dijkstra shortest path

--- algorithms/Dijkstra.js
+++ algorithms/Dijkstra.js
@@ -1,9 +1,9 @@
-const {Graph, Node} = require('../data_structures/Graph')
+const { Graph, Node } = require('../data_structures/Graph')
 const PriorityQueue = require('../data_structures/MinHeap')
 
 //init dummy data
 let graph = new Graph()
-graph.nodes = (new Array(6)).fill(0).map((_, i) => new Node(i))
+graph.nodes = (new Array(6)).fill(0).map((_, i) => new Node({ name: `Node${i}`, value: i }))
 graph.nodes[0].children = [
     graph.nodes[1],
     graph.nodes[4],
@@ -22,62 +22,103 @@ graph.nodes[3].children = [
 ]
 
 const links = [{
-    source: 0,
-    dest: 1,
+    source: "Node" + 0,
+    dest: "Node" + 1,
     weight: 1
 }, {
-    source: 0,
-    dest: 4,
+    source: "Node" + 0,
+    dest: "Node" + 4,
     weight: 7
 }, {
-    source: 0,
-    dest: 5,
+    source: "Node" + 0,
+    dest: "Node" + 5,
     weight: 1
 }, {
-    source: 1,
-    dest: 3,
+    source: "Node" + 1,
+    dest: "Node" + 3,
     weight: 1
 }, {
-    source: 1,
-    dest: 4,
+    source: "Node" + 1,
+    dest: "Node" + 4,
     weight: 5
 }, {
-    source: 2,
-    dest: 1,
+    source: "Node" + 2,
+    dest: "Node" + 1,
     weight: 3
 }, {
-    source: 3,
-    dest: 2,
+    source: "Node" + 3,
+    dest: "Node" + 2,
     weight: 2
 }, {
-    source: 3,
-    dest: 4,
+    source: "Node" + 3,
+    dest: "Node" + 4,
     weight: 2
 }]
 
 let start = graph.nodes[0], end = graph.nodes[4]
 
 //Solve
-let queue = new PriorityQueue()
 
-const shortestPath = (graph, links, start, end) => {
-    
+//Fill Queue
+const init = (graph, start) => {
+    let pq = new PriorityQueue(n => n.value)
+    let graphNodes = {} //map
+    graph.nodes.forEach(node => {
+        graphNodes[node.data.name] = pq.add({
+            node: node,
+            value: node.data.name === start.data.name ? 0 : Infinity
+        })
+    })
+
+    return {
+        pq: pq,
+        graphNodes: graphNodes
+    }
 }
 
-const traverseGraph = (root) => {
-    if(!node.visited) {
-        if(root.children.length) {
-            root.children.forEach(child => {
-                traverseGraph(child)
-            })
+const printPath = (path, start, end) => {
+    let pathStr = "No path found"
+    for (let i = path.length - 1; i >= 0; i--) {
+        if (path[i].dest.data.name === end.data.name) { //found end
+            let dest = end.data.name
+            let source = path[i].source.data.name
+            pathStr = ` -> ${dest} at cost ${path[i].cost}`
+            while (i > 0) {
+                i--
+                if (path[i].dest.data.name !== source) continue
+                dest = path[i].dest.data.name
+                source = path[i].source.data.name
+                pathStr = ` -> ${dest}` + pathStr
+            }
+
+            pathStr = "Shortest path: " + start.data.name + pathStr
         }
     }
 
-    if (node !== start) { //calculate weights
-        if (queue.nodes.indexOf())
+    return pathStr
+}
+
+const shortestPath = (graph, links, start, end) => {
+    let { pq, graphNodes } = init(graph, start)
+    let path = []
+
+    while (!pq.isEmpty()) {
+        let vertex = pq.popMin()
+        let edges = vertex.node.children
+
+        edges.forEach(edge => {
+            let weight = links.find(l => l.source === vertex.node.data.name && l.dest === edge.data.name).weight
+            let oldWeight = graphNodes[edge.data.name].value
+            let newWeight = graphNodes[vertex.node.data.name].value + weight
+
+            if (newWeight < oldWeight) {
+                path.push({ source: vertex.node, dest: edge, cost: newWeight })
+                pq.update(graphNodes[edge.data.name], node => node.value = newWeight)
+            }
+        })
     }
 
-    node.visited = true
+    return printPath(path, start, end)
 }
 
 console.log(shortestPath(graph, links, start, end))
--- data_structures/MinHeap.js
+++ data_structures/MinHeap.js
@@ -1,14 +1,27 @@
-function MinHeap (valueFunc) {
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+    })
+}
+
+function MinHeap(valueFunc) {
     this.nodes = []
     this.getValue = node => node
     if (valueFunc) this.getValue = valueFunc
 
     let swap = (pos1, pos2) => {
-        let node1 = JSON.parse(JSON.stringify(this.nodes[pos1]))
-        let node2 = JSON.parse(JSON.stringify(this.nodes[pos2]))
-
-        this.nodes[pos1] = node2
-        this.nodes[pos2] = node1
+        if (pos1 < pos2) {
+            let posTemp = pos2
+            pos2 = pos1
+            pos1 = posTemp
+        }
+        //copy
+        this.nodes.splice(pos1 + 1, 0, this.nodes[pos2])
+        this.nodes.splice(pos2 + 1, 0, this.nodes[pos1])
+        //remove
+        this.nodes.splice(pos1 + 1, 1)
+        this.nodes.splice(pos2, 1)
     }
 
     let bubbleUp = n => {
@@ -44,10 +57,10 @@ function MinHeap (valueFunc) {
 
             if (!swapPos) {
                 if (this.getValue(child) < this.getValue(parent))
-                swapPos = secondChildPos
+                    swapPos = secondChildPos
             } else {
                 if (this.getValue(child) < this.getValue(this.nodes[firstChildPos]))
-                swapPos = secondChildPos
+                    swapPos = secondChildPos
             }
         }
 
@@ -65,28 +78,65 @@ function MinHeap (valueFunc) {
 
     this.popMin = () => {
         let min = this.nodes[0]
-        this.nodes[0] = this.nodes.pop()
+        if (this.nodes.length === 1)
+            this.nodes.pop()
+        else this.nodes[0] = this.nodes.pop()
         bubbleDown(0)
 
         return min
     }
 
     this.add = node => {
+        node._id = uuidv4() //assign unique id
         this.nodes.push(node)
-        bubbleUp(this.nodes.length-1)
+        bubbleUp(this.nodes.length - 1)
+
+        return node
     }
+
+    this.remove = node => {
+        for (let i = 0; i < this.nodes.length; i++) {
+            if (this.nodes[i]._id !== node._id)
+                continue
+
+            let end = this.nodes.pop()
+            if (i == this.nodes.length - 1)
+                break
+
+            this.nodes[i] = end
+            bubbleUp(i)
+            bubbleDown(i)
+            break
+        }
+    }
+
+    this.update = (node, ufunc) => {
+        let index = 0
+        for (index; index < this.nodes.length; index++)
+            if (this.nodes[index]._id === node._id) break
+
+        ufunc(this.nodes[index])
+        swap(index, this.nodes.length - 1)
+        bubbleUp(this.nodes.length - 1)
+        bubbleDown(index)
+    }
+
+    this.isEmpty = () => this.nodes.length === 0
 }
 
 // let minHeap = new MinHeap(node => node.value)
-// minHeap.add({name: "node 0", value: 7})
-// minHeap.add({name: "node 1", value: 31})
-// minHeap.add({name: "node 2", value: 2})
-// minHeap.add({name: "node 3", value: 4})
-// minHeap.add({name: "node 4", value: 3})
-// minHeap.add({name: "node 5", value: 10})
-// minHeap.add({name: "node 6", value: 0})
+// let node0 = minHeap.add({name: "node 0", value: 7})
+// let node1 = minHeap.add({name: "node 1", value: 31})
+// let node2 = minHeap.add({name: "node 2", value: 2})
+// let node3 = minHeap.add({name: "node 3", value: 4})
+// let node4 = minHeap.add({name: "node 4", value: 3})
+// let node5 = minHeap.add({name: "node 5", value: 10})
+// let node6 = minHeap.add({name: "node 6", value: 0})
 // minHeap.popMin()
 // minHeap.popMin()
 // minHeap.popMin()
+// minHeap.remove(node5)
+// minHeap.remove(node6)
+// minHeap.update(node1, node => node.value = 0)
 
 module.exports = MinHeap
