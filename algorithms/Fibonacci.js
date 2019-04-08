6a78c04664b15a3ffeb26d014ec453ca55e8f2a3
mjovani <mjovani@helius-software.com>
2019-04-08 20:42:27 +0200

    Added Fibonacci sequence

---
+++ algorithms/Fibonacci.js
@@ -0,0 +1,34 @@
+//Fibonacci sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+//Recursive
+const fib = (n) => {
+    if (n == 0 || n == 1)
+        return n
+    else
+        return fib(n - 1) + fib(n - 2)
+}
+
+const fibSeqRec = (n) => {
+    let seq = ""
+    for (let i = 0; i <= n; i++) {
+        seq += fib(i) + " "
+    }
+
+    return seq
+}
+
+//iterative
+const fibSeqIter = (n) => {
+    let fib = [0, 1],
+        seq = "0 1"
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+        seq += " " + fib[i]
+    }
+
+    return seq
+}
+
+console.log(fibSeqRec(9))
+console.log(fibSeqIter(9))
