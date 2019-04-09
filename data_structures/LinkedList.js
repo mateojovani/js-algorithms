b367fa4d7010a43bbaa9dff3b5f422a144bed754
mjovani <mjovani@helius-software.com>
2019-04-09 10:54:11 +0200

    Linked List prev index fix

--- data_structures/LinkedList.js
+++ data_structures/LinkedList.js
@@ -48,6 +48,8 @@ function List () {
                 node.next = current.next
                 current.next = node
                 node.prev = current
+                if (node.next && node.next.prev)
+                    node.next.prev = node
             }
         } else { //add to tail
             while (current.next)
@@ -92,19 +94,44 @@ function List () {
     }
 }
 
-let list = new List()
-list.add(1)
-list.add(2)
-list.add(3)
-list.add(5)
-list.add(4, 2)
-list.add(0, 0)
-list.add(20, 6)
-list.add(-1, 0)
-list.add(21)
-list.remove(7)
-list.remove(0)
-list.remove(9)
-list.add(6)
+// let list = new List()
 
-console.log(list.toArray())
+// list.add(1)
+// list.add(2)
+// list.add(3)
+// list.add(5)
+// list.add(4, 2)
+// list.add(0, 0)
+// list.add(20, 6)
+// list.add(-1, 0)
+// list.add(21)
+// list.remove(7)
+// list.remove(0)
+// list.remove(9)
+// list.add(6)
+
+// console.log(list.toArray())
+
+// const getListTail = (list) => {
+//     let cur = list.head
+//     while (cur.next) {
+//         cur = cur.next
+//     }
+
+//     return cur
+// }
+
+// const buildReversed = (list) => {
+//     let tail = getListTail(list)
+//     let reversed = new List()
+
+//     while (tail.prev) {
+//         reversed.add(tail.data)
+//         tail = tail.prev
+//     }
+
+//     reversed.add(tail.data)
+//     return reversed
+// }
+
+// console.log(buildReversed(list).toArray())
